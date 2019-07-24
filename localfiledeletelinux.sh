1f8f4e428b16919b2d6bc983cd12371374964125
Jenni Hartman <40398394+jhartmanNU@users.noreply.github.com>
2019-07-24 10:24:35 -0500

    Update localfiledeletelinux.sh

--- localfiledeletelinux.sh
+++ localfiledeletelinux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #deletes files (not folders) older than 90 days recursively, confirm directory path before executing!
-#mtime checks for file data modified n*24 hours ago, rounding can affect this, so use 91 for command
+#mtime checks for file data modified n*24 hours ago, rounding can affect this, so use days+1 for command
 #run script as a cron to automate
 
 #dryrun
-find /path/to/files/ -type f -name '*' -mtime +90 > dryrun.txt
+find /path/to/files/ -type f -name '*' -mtime +91 > dryrun.txt
 
 #actual run
-#find /path/to/files/ -type f -name '*' -mtime +90 -exec rm {} \;
+#find /path/to/files/ -type f -name '*' -mtime +91 -exec rm {} \;
